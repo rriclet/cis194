3207d7649b0bc52c24a7165d3444929edc78448a
Robin Riclet <robinriclet@gmail.com>
2020-04-12 22:18:48 +0200

    Changed if to guard

--- src/Week3/Golf.hs
+++ src/Week3/Golf.hs
@@ -8,6 +8,6 @@ skips :: [a] -> [[a]]
 skips l = snd $ mapAccumL (\x y -> (x + 1, nthItems x y)) 1 $ take (length l) (repeat l) 
 
 nthItems :: Int -> [a] -> [a]
-nthItems n l = if n > 0 && n <= length l 
-               then (take 1 $ drop (n-1) l) ++ nthItems n (drop n l)
-               else []
+nthItems n l
+    | n > 0 && n <= length l = (take 1 $ drop (n-1) l) ++ nthItems n (drop n l)
+    | otherwise              = []
